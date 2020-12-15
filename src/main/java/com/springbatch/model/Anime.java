ded1b8b40e17ddf2ff983a30834c720b7692cd45
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-15 01:28:52 -0500

    getters and setters

--- src/main/java/com/springbatch/model/Anime.java
+++ src/main/java/com/springbatch/model/Anime.java
@@ -15,6 +15,8 @@ public class Anime {
     private double rating;
     private int members;
 
+    public Anime() {}
+
     public Anime(String anime_id, String name, String genre, String type, int episodes, double rating, int members) {
         this.anime_id = anime_id;
         this.name = name;
@@ -25,4 +27,60 @@ public class Anime {
         this.members = members;
     }
 
+
+    public String getAnime_id() {
+        return anime_id;
+    }
+
+    public void setAnime_id(String anime_id) {
+        this.anime_id = anime_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(int episodes) {
+        this.episodes = episodes;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getMembers() {
+        return members;
+    }
+
+    public void setMembers(int members) {
+        this.members = members;
+    }
 }
