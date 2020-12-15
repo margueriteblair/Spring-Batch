85cdf021a8ab575bf74bce9dc7ccc54a96f9a0c5
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-15 02:19:12 -0500

    built a processor that returns the animee

--- src/main/java/com/springbatch/batch/Processor.java
+++ src/main/java/com/springbatch/batch/Processor.java
@@ -5,10 +5,14 @@ import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class Processor implements ItemProcessor<Anime, Anime> {
+
     @Override
     public Anime process(Anime anime) throws Exception {
-        return null;
+        return anime;
     }
 }
