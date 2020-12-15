b67e9455a9d0248c955b22ce6017550111f87d83
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-15 02:23:40 -0500

    must use save all command

--- src/main/java/com/springbatch/batch/DBWriter.java
+++ src/main/java/com/springbatch/batch/DBWriter.java
@@ -1,15 +1,22 @@
 package com.springbatch.batch;
 
 import com.springbatch.model.Anime;
+import com.springbatch.repository.AnimeRepository;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class DBWriter implements ItemWriter<Anime> {
-    @Override
-    public void write(List<? extends Anime> list) throws Exception {
 
+    @Autowired
+    private AnimeRepository animeRepository;
+
+    @Override
+    public void write(List<? extends Anime> listOfAnimes) throws Exception {
+        System.out.println("Data being saved for animes: " + listOfAnimes);
+        animeRepository.saveAll(listOfAnimes);
     }
 }
