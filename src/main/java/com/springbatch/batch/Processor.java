f0db2f67f6c664a9ed696cfb4cdee9eb003915e9
Marguerite Blair <marguerite.blair@gmail.com>
2020-12-16 15:48:35 -0500

    cleanup

--- src/main/java/com/springbatch/batch/Processor.java
+++ src/main/java/com/springbatch/batch/Processor.java
@@ -2,11 +2,8 @@ package com.springbatch.batch;
 
 import com.springbatch.model.Anime;
 import org.springframework.batch.item.ItemProcessor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class Processor implements ItemProcessor<Anime, Anime> {
